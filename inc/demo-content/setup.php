a09b3c1f4d80b3d3da03a74ccf35ba688c0960d7
rabindratharu <rabindra.tharu.np@gmail.com>
2017-04-27 13:47:53 +0545

    fixed one click demo importer for front page sidebar widgets

--- inc/demo-content/setup.php
+++ inc/demo-content/setup.php
@@ -6,13 +6,11 @@
  */
 
 /*Import content data*/
-
-/*Import content data*/
 if ( ! function_exists( 'courtyard_import_files' ) ) :
 function courtyard_import_files() {
     return array(
         array(
-            'import_file_name'             => 'Courtyard Demo',
+            'import_file_name'             => 'Courtyard',
             'local_import_file'            => trailingslashit( get_template_directory() ) . 'inc/demo-content/demo-content.xml',
             'local_import_widget_file'     => trailingslashit( get_template_directory() ) . 'inc/demo-content/demo-widgets.wie',
             'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'inc/demo-content/demo-customizer.dat',
@@ -24,13 +22,36 @@ add_filter( 'pt-ocdi/import_files', 'courtyard_import_files' );
 endif;
 
 /**
+ * Create the homepage to hold the widgets, before the import
+ */
+
+if ( ! function_exists( 'courtyard_set_after_import_mods' ) ) :
+function courtyard_before_content_import( $selected_import ) {
+
+    if ( 'Courtyard' === $selected_import['import_file_name'] ) {
+        $homepage = array(
+          'post_title'    => 'Home',
+          'post_type'     => 'page',  
+          'post_content'  => '',
+          'post_status'   => 'publish',
+        );
+         
+        wp_insert_post( $homepage );
+        $front_page = get_page_by_title( 'Home' );
+        update_post_meta( $front_page -> ID, '_wp_page_template', 'page-templates/template-front-page.php' );
+    }
+}
+add_action( 'pt-ocdi/before_content_import', 'courtyard_before_content_import' );
+endif;
+
+/**
  * Define actions that happen after import
  */
 
 if ( ! function_exists( 'courtyard_set_after_import_mods' ) ) :
 function courtyard_set_after_import_mods( $selected_import ) {
  
-    if ( 'Courtyard Demo' === $selected_import['import_file_name'] ) {
+    if ( 'Courtyard' === $selected_import['import_file_name'] ) {
         
         //Assign the menu
         $primary_menu = get_term_by( 'name', 'Main Menu', 'nav_menu' );
@@ -46,9 +67,6 @@ function courtyard_set_after_import_mods( $selected_import ) {
         update_option( 'show_on_front', 'page' );
         update_option( 'page_on_front', $front_page -> ID );
         update_option( 'page_for_posts', $blog_page -> ID );
-
-        //Assign the Front Page template
-        update_post_meta( $front_page -> ID, '_wp_page_template', 'page-templates/template-front-page.php' );
     }     
 }
 add_action( 'pt-ocdi/after_import', 'courtyard_set_after_import_mods' );
--- js/custom.js
+++ js/custom.js
@@ -38,81 +38,6 @@ jQuery(document).ready(function() {
 		jQuery('.pt-navbar').removeClass('pt-home-navbar');
 		jQuery('.pt-header-sep').removeClass('pt-header-sep-hide');
 	}
-	
-	// Sliders
-	var dt_front_slider = new Swiper ('.pt-hero-image-slider',{
-		nextButton: '.pt-hero-slider-nav .pt-arrow-right',
-		prevButton: '.pt-hero-slider-nav .pt-arrow-left',
-		slidesPerView: 1,
-		spaceBetween: 0,
-		loop: true,
-		autoplay: 3000,
-		speed: 1200,
-		effect: 'fade',
-		preventClicks: false,
-		touchEventsTarget: 'swiper-wrapper',
-		paginationClickable: true,
-
-		onTransitionStart: function(slider) {
-
-			var active_slide = slider.activeIndex;
-
-			setTimeout(function () {
-				jQuery('.swiper-slide').eq(active_slide).find('.pt-hero-image-cont header').fadeIn().addClass('animated fadeInUp');
-			},400);
-
-			setTimeout(function () {
-				jQuery('.swiper-slide').eq(active_slide).find('.pt-hero-image-cont article').fadeIn().addClass('animated fadeInUp');
-			},600);
-		},
-
-		onSlideChangeEnd: function(slider) {
-
-			var next_slide = slider.activeIndex+1;
-			var previous_slide = slider.activeIndex-1;
-
-			jQuery('.swiper-slide').eq(next_slide).find('.pt-hero-image-cont header').hide();
-			jQuery('.swiper-slide').eq(previous_slide).find('.pt-hero-image-cont header').hide();
-
-			jQuery('.swiper-slide').eq(next_slide).find('.pt-hero-image-cont article').hide();
-			jQuery('.swiper-slide').eq(previous_slide).find('.pt-hero-image-cont article').hide();
-		}
-	});
-
-	var courtyard_window_width = jQuery(window).width();
-
-	var courtyard_services_slider_num = 3;
-	var courtyard_services_slider_per_col = 2;
-
-	if ( courtyard_window_width <= 992 ) {
-		var courtyard_services_slider_num = 2;
-	}
-
-	if ( courtyard_window_width <= 580 ) {
-		var courtyard_services_slider_per_col = 1;
-		var courtyard_services_slider_num = 1;
-	}
-
-	var pt_services_slider = new Swiper('.pt-services-slider', {
-		nextButton: '.pt-services-more .pt-arrow-right',
-		prevButton: '.pt-services-more .pt-arrow-left',
-		spaceBetween: 0,
-		preventClicks: false,
-		slidesPerView: courtyard_services_slider_num,
-		slidesPerColumn: courtyard_services_slider_per_col,
-		touchEventsTarget: 'swiper-wrapper',
-		speed: 800
-	});
-
-	var pt_rooms_slider = new Swiper('.pt-rooms-slider', {
-		nextButton: '.pt-rooms-more .pt-arrow-right',
-		prevButton: '.pt-rooms-more .pt-arrow-left',
-		spaceBetween: 30,
-		preventClicks: false,
-		slidesPerView: courtyard_services_slider_num,
-		touchEventsTarget: 'swiper-wrapper',
-		speed: 800
-	});
 
 	// Back to Top
 	if (jQuery('#back-to-top').length) {
@@ -134,6 +59,131 @@ jQuery(document).ready(function() {
 			jQuery('html,body').animate({
 				scrollTop: 0
 			}, 800);
+		});
+	}
+});
+
+jQuery(window).load(function() {
+	if(typeof Swiper === 'function'){
+		var courtyard_window_width = jQuery(window).width();
+
+		var courtyard_services_slider_num = 3;
+
+		if ( courtyard_window_width <= 992 ) {
+			var courtyard_services_slider_num = 2;
+		}
+
+		if ( courtyard_window_width <= 580 ) {
+			var courtyard_services_slider_num = 1;
+		}
+
+		// Hero Slider
+		jQuery('.pt-image-slider-section').each(function(index, element){
+			var container  = jQuery(this).find('.pt-hero-image-slider');
+			var nextButton = jQuery(this).find('.pt-hero-slider-nav .pt-arrow-right');
+			var prevButton = jQuery(this).find('.pt-hero-slider-nav .pt-arrow-left');
+
+			var dt_front_slider = new Swiper (container,{
+				nextButton: nextButton,
+				prevButton: prevButton,
+				slidesPerView: 1,
+				spaceBetween: 0,
+				loop: true,
+				autoplay: 3000,
+				speed: 1200,
+				effect: 'fade',
+				preventClicks: false,
+				touchEventsTarget: 'swiper-wrapper',
+				paginationClickable: true,
+
+				onTransitionStart: function(slider) {
+
+					var active_slide = slider.activeIndex;
+
+					setTimeout(function () {
+						jQuery('.swiper-slide').eq(active_slide).find('.pt-hero-image-cont header').fadeIn().addClass('animated fadeInUp');
+					},400);
+
+					setTimeout(function () {
+						jQuery('.swiper-slide').eq(active_slide).find('.pt-hero-image-cont article').fadeIn().addClass('animated fadeInUp');
+					},600);
+				},
+
+				onSlideChangeEnd: function(slider) {
+
+					var next_slide = slider.activeIndex+1;
+					var previous_slide = slider.activeIndex-1;
+
+					jQuery('.swiper-slide').eq(next_slide).find('.pt-hero-image-cont header').hide();
+					jQuery('.swiper-slide').eq(previous_slide).find('.pt-hero-image-cont header').hide();
+
+					jQuery('.swiper-slide').eq(next_slide).find('.pt-hero-image-cont article').hide();
+					jQuery('.swiper-slide').eq(previous_slide).find('.pt-hero-image-cont article').hide();
+				}
+			});
+		});
+
+		// Service Slider
+		jQuery('.pt-service-section').each(function(index, element){
+			var container  = jQuery(this).find('.pt-services-slider');
+			var slideCount = jQuery(this).find('.swiper-slide').length;
+			var nextButton = jQuery(this).find('.pt-services-more .pt-arrow-right');
+			var prevButton = jQuery(this).find('.pt-services-more .pt-arrow-left');
+
+			if ( slideCount < 6 ) {
+				var courtyard_services_slider_per_col = 1;
+			}
+			if ( slideCount >= 6 ) {
+				var courtyard_services_slider_per_col = 2;
+			}
+			if ( courtyard_window_width <= 580 ) {
+				var courtyard_services_slider_per_col = 1;
+			}
+
+			var pt_services_slider = new Swiper(container, {
+				nextButton: nextButton,
+				prevButton: prevButton,
+				spaceBetween: 0,
+				preventClicks: false,
+				slidesPerView: courtyard_services_slider_num,
+				slidesPerColumn: courtyard_services_slider_per_col,
+				touchEventsTarget: 'swiper-wrapper',
+				speed: 800
+			});
+		});
+
+		// Room Slider
+		jQuery('.pt-rooms-section').each(function(index, element){
+			var container  = jQuery(this).find('.pt-rooms-slider');
+			var nextButton = jQuery(this).find('.pt-rooms-more .pt-arrow-right');
+			var prevButton = jQuery(this).find('.pt-rooms-more .pt-arrow-left');
+
+			var pt_rooms_slider = new Swiper(container, {
+				nextButton: nextButton,
+				prevButton: prevButton,
+				spaceBetween: 30,
+				preventClicks: false,
+				slidesPerView: courtyard_services_slider_num,
+				touchEventsTarget: 'swiper-wrapper',
+				speed: 800
+			});
+		});
+
+		// Testimonial Slider
+		jQuery('.pt-testimonials-section').each(function(index, element){
+			var container  = jQuery(this).find('.pt-testimonials-slider');
+			var nextButton = jQuery(this).find('.pt-testimonials-more .pt-arrow-right');
+			var prevButton = jQuery(this).find('.pt-testimonials-more .pt-arrow-left');
+
+			var pt_testimonials_slider = new Swiper(container, {
+				nextButton: nextButton,
+				prevButton: prevButton,
+				spaceBetween: 30,
+				preventClicks: false,
+				slidesPerView: courtyard_services_slider_num,
+				touchEventsTarget: 'swiper-wrapper',
+				speed: 800
+			});
 		});
 	}
 });
